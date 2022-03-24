b7e782b9b587fa712d6f3c0567080e4bccfc3aaa
Zicheng.S <zichengshan@gmail.com>
2022-03-24 12:06:06 -0700

    interactive in counting

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.getElementById("count-el").innerText = 6
+
+let countEl  = document.getElementById("count-el")
+let count = 0
+function increaseCount(){
+    count++;
+    countEl.innerText = count
+}
+
+
+
