498fc278d609f3a78f26549a4d366e86fcd2217f
Zicheng.S <zichengshan@gmail.com>
2022-03-24 14:42:34 -0700

    Finish the counter

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-// document.getElementById("count-el").innerText = 6
-
 let countEl  = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
+
 function increaseCount(){
     count++;
     countEl.innerText = count
 }
+
+
+function decreaseCount() {
+    count--;
+    if(count < 0){
+        count = 0
+    }
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent +=  countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
 
 
 
